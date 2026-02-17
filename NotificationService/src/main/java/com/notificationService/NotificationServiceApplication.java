667ce97f713cfb3c3d962f64f7b161896061bef0
Prakhar sakhare~ <prakharsakhare2226@gmail.com>
2026-02-18 01:48:54 +0530

    minor update

--- NotificationService/src/main/java/com/notificationService/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/notificationService/NotificationServiceApplication.java
@@ -2,9 +2,11 @@ package com.notificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
